9210b3016dbf6366b0a45b4ae0aa40918dee6f7f
Александр Жалыбин <alex453528368@yandex.ru>
2025-11-27 12:30:46 +0300

    добавил тест 1.1 и 1.2, исправил тесты 4, 6. исправил README

--- tests.py
+++ tests.py
@@ -4,15 +4,26 @@ from main import BooksCollector
 
 class TestBooksCollector:
 
-    # Тест 1 добавляет новую книгу в словарь без указания жанра
+    # Тест 1.1 добавляет новую книгу в словарь без указания жанра (валидные граничные значения)
     @pytest.mark.parametrize('book_name', [
-        'Тестирование ПО',
-        'Harry Potter', 
-        '1 + 1'
+        'А',                    # 1 символ
+        'Книга',  # 5 символов
+        '40 символов (12345678912345678912134541)'  # 40 символов
     ])
     def test_add_new_book_valid_name_book_added(self, book_name, collection):
         collection.add_new_book(book_name)
         assert book_name in collection.get_books_genre()
+
+    # Тест 1.2 добавляет новую книгу в словарь без указания жанра (невалидные граничные значения)
+    @pytest.mark.parametrize('book_name', [
+        '',                     # 0 символов
+        '41 символов (123456789123456789121345411)',  # 41 символ
+        '50 символов (50 символов (123456789123456789121345411123546789))' # 50 символов
+
+    ])
+    def test_add_new_book_invalid_name_not_added(self, book_name, collection):
+        collection.add_new_book(book_name)
+        assert book_name not in collection.get_books_genre()
 
     # Тест 2 устанавливаем жанр книги, если книга есть в books_genreи её жанр входит в список genre
     @pytest.mark.parametrize('book_name, genre, expected_genre', [
@@ -33,7 +44,11 @@ class TestBooksCollector:
 
     # Тест 4 выводим список книг с определённым жанром.
     def test_get_books_with_specific_genre_existing_genre_returns_books(self, collection_books):
-        assert collection_books.get_books_with_specific_genre('Ужасы') == ['Оно']
+        # Тут вызвал значение из фикстуры
+        horror_books = collection_books.get_books_with_specific_genre('Ужасы')
+        # Тут проверил что каждая найденная книга из фикстуры имеет жанр 'Ужасы'
+        for book in horror_books:
+            assert collection_books.get_book_genre(book) == 'Ужасы'
 
     # Тест 5 выводит текущий словарь books_genre
     def test_get_books_genre_with_books_returns_dict(self, collection):
@@ -44,9 +59,12 @@ class TestBooksCollector:
 
     # Тест 6 возвращает книги, которые подходят детям.
     def test_get_books_for_children_mixed_genres_returns_child_friendly(self, collection_books):
+        # Тут вызвал значение из фикстуры
         children_books = collection_books.get_books_for_children()
-        assert len(children_books) == 3 and children_books == ['Гарри Поттер', 'Ну погоди!', 'Ирония судьбы']
-
+        # Тут проверяем что имеет жанр не из age_rating (не 'Ужасы' или 'Детективы')
+        for book in children_books:
+            book_genre = collection_books.get_book_genre(book)
+            assert book_genre not in collection_books.genre_age_rating
 
     # Тест 7 добавляет книгу в избранное. Книга должна находиться в словаре books_genre.
     def test_add_book_in_favorites_existing_book_added(self, collection):
